191332d365133bfc6a0505e705bf6310023673a3
Evgeny Eroshin <eee333@yandex.ru>
2022-03-19 02:07:49 +0300

    Create autofill user in SelectionCreateView

--- ads/models.py
+++ ads/models.py
@@ -34,7 +34,7 @@ class Ad(models.Model):
 
 class Selection(models.Model):
     name = models.CharField(max_length=50)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     items = models.ManyToManyField(Ad)
 
     class Meta:
